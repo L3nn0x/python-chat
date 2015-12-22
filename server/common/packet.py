a060e3f8be45b584bcad7f1d745a9d086d2c37b1
L3nn0x <dragon;super@laposte.net>
2015-12-22 03:43:32 +0100

    Renamed a few variables and added a few comments

--- server/common/packet.py
+++ server/common/packet.py
@@ -1,7 +1,7 @@
 import pickle
 
-SIZE = 4096
-SIZESIZE = 2
+SIZE_NETWORK_PACKET = 4096  # max network frame size sent at once
+SIZE_LENGTH = 2             # max total size for one Packet: 256^SIZE_LENGTH
 
 class   Packet:
     def __init__(self, packetType, *args, **kwargs):
@@ -21,32 +21,37 @@ class   Packet:
 
     @staticmethod
     def deserialize(data):
-        return pickle.loads(data)
+        try:
+            return pickle.loads(data)
+        except:
+            return None
 
+# Return True if the Packet was sent, False otherwise
 def sendPacket(sock, packet):
     data = packet.serialize()
     size = len(data)
-    if size >= pow(256, SIZESIZE):
+    if size >= pow(256, SIZE_LENGTH):
         return False
     packets = []
-    while len(data) > SIZE:
-        packets.append(data[:SIZE])
-        data = data[SIZE:]
+    while len(data) > SIZE_NETWORK_PACKET:
+        packets.append(data[:SIZE_NETWORK_PACKET])
+        data = data[SIZE_NETWORK_PACKET:]
     packets.append(data)
-    sock.sendall(size.to_bytes(SIZESIZE, byteorder="big"))
+    sock.sendall(size.to_bytes(SIZE_LENGTH, byteorder="big"))
     for packet in packets:
         sock.sendall(packet)
     return True
 
+# Return None if no packet was able to be received, otherwise returns the correctly formated Packet
 def recvPacket(sock):
-    size = int.from_bytes(sock.recv(SIZESIZE), byteorder="big")
+    size = int.from_bytes(sock.recv(SIZE_LENGTH), byteorder="big")
     if size < 1:
         return None
-    if size < SIZE:
+    if size < SIZE_NETWORK_PACKET:
         return Packet.deserialize(sock.recv(size))
     data = bytes()
-    while len(data) < size and size - len(data) >= SIZE:
-        data = data + sock.recv(SIZE)
+    while len(data) < size and size - len(data) >= SIZE_NETWORK_PACKET:
+        data = data + sock.recv(SIZE_NETWORK_PACKET)
     if size > len(data):
         data = data + sock.recv(size - len(data))
     return Packet.deserialize(data)
