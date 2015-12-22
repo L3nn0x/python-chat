6d58694f9a53f2ba2f2b635323defa67fdd3d659
L3nn0x <dragon;super@laposte.net>
2015-12-22 14:06:54 +0100

    Added new tests in client

--- client/main.py
+++ client/main.py
@@ -1,10 +1,25 @@
 import socket
 import select
+from common.protocol import *
 from common.packet import *
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(('127.0.0.1', 1234))
-a = Packet("test", [i for i in range(256*10)])
-print(sendPacket(sock, a))
-print(sendPacket(sock, Packet("profile", 1234, "tada", nick=12, channel="general")))
+print(sendPacket(sock, Packet("plop")))
+print(sendPacket(sock, hello()))
+print(sendPacket(sock, hello()))
+print(sendPacket(sock, hello()))
+print(sendPacket(sock, hello()))
+print(sendPacket(sock, hello()))
+print(sendPacket(sock, hello()))
+print(sendPacket(sock, hello()))
+print(sendPacket(sock, hello()))
+print(sendPacket(sock, hello()))
+print(sendPacket(sock, hello()))
+print(sendPacket(sock, hello()))
+print(sendPacket(sock, hello()))
+print(sendPacket(sock, hello()))
+print(sendPacket(sock, hello()))
+print(sendPacket(sock, hello()))
+print(recvPacket(sock))
 sock.close()
--- server/client.py
+++ server/client.py
@@ -1,24 +1,65 @@
 from threading import Thread
 import select
 from common.packet import *
+from common.protocol import *
+from collections import deque
 
 class   Client(Thread):
+    _CONNECT = 0
+    _LOGIN = 1
+    _NORMAL = 2
     def __init__(self, sock, addr):
         super().__init__()
         self.sock = sock
         self.addr = addr
+        self.chans = ['general']
         self.connected = True
+        self.state = Client._CONNECT
+        self.states = {
+                Client._CONNECT: self.connect,
+                Client._LOGIN: self.login,
+                Client._NORMAL: self.normal,
+            }
+        self.wpackets = deque()
 
     def run(self):
         while self.connected:
-            rlist = select.select([self.sock], [], [], 1)[0]
+            rlist, wlist, xlist = select.select([self.sock], [self.sock], [])
             if len(rlist):
                 packet = recvPacket(self.sock)
                 if not packet:
                     self.stop()
                 else:
+                    print("Client", self.addr, "sent:")
                     print(packet)
+                    self.crunch(packet)
+            elif len(wlist) and len(self.wpackets):
+                if not sendPacket(self.sock, self.wpackets.popleft()):
+                    self.stop()
+
+    def send(self, packet):
+        self.wpackets.append(packet)
+
+    def crunch(self, packet):
+        try:
+            self.states[self.state](packet)
+        except Exception as e:
+            print("Unknown error:", e)
+            self.stop()
+
+    def connect(self, packet):
+        if packet.packetType != HELLO:
+            self.stop()
+            return
+        self.send(hello())
+    
+    def login(self, packet):
+        pass
+
+    def normal(self, packet):
+        pass
 
     def stop(self):
+        print("Client disconnected:", self.addr)
         self.connected = False
         self.sock.close()
