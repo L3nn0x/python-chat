f7dc6fcafd4fbcfa42c2ae223b41cc4cf9c11c59
L3nn0x <dragon;super@laposte.net>
2015-12-22 12:43:42 +0100

    Added first iteration of protocol

---
+++ server/common/protocol.py
@@ -0,0 +1,40 @@
+from common.packet import Packet
+
+def hello():
+    return Packet(HELLO)
+
+def ok():
+    return Packet(OK)
+
+def nok():
+    return Packet(NOK)
+
+def credentials(login, password):
+    return Packet(CREDENTIALS, login=login, password=password)
+
+def channels(*channels):
+    return Packet(CHAN, *channels)
+
+def msg(source, dest, msg):
+    return Packet(MSG, source=source, destination=dest, data=msg)
+
+# protocol
+HELLO = "HELLO"                 # first packet sent
+CREDENTIALS = "CREDENTIALS"     # with login=<> and password=<> (encrypted)
+OK = "OK"                       # ok packet
+NOK = "NOK"                     # not ok packet
+CHAN = "CHAN"                   # send channel names (all of them) and people inside
+MSG = "MSG"                     # send a msg with source, dest and data
+
+"""Client connects: 
+    - HELLO
+                        - HELLO
+    - CREDENTIALS
+                        - OK|NOK
+                        - CHAN
+    - OK|NOK
+                        - (if NOK resend, else wait)
+    - MSG
+                        - OK|NOK
+    - (if NOK resend, else continue)
+"""
