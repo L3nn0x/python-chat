2edffc5e9e4bde4fd98a0db724fedc1fc5d7a3c0
L3nn0x <dragon;super@laposte.net>
2016-01-05 23:31:00 +0100

    Added automatic scrolling

--- client/verticalscrolledframe.py
+++ client/verticalscrolledframe.py
@@ -1,16 +1,22 @@
 import tkinter as tk
 
 class   VerticalScrolledFrame(tk.Frame):
-    def __init__(self, parent, *args, **kwargs):
+    def __init__(self, parent, scrollAuto=False, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
         scrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
         scrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
-        canvas = tk.Canvas(self, bd=0, highlightthickness=0, yscrollcommand=scrollbar.set)
+        self.canvas = canvas = tk.Canvas(self, bd=0, highlightthickness=0, yscrollcommand=scrollbar.set)
         canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
-        scrollbar.config(command=canvas.yview)
+        self.interior = interior = tk.Frame(canvas)
+        self.down = True
+        self.scrollAuto = scrollAuto
+        def _scroll(*args):
+            canvas.yview(*args)
+            if int(canvas.yview()[1]) == 1:
+                self.down = True
+        scrollbar.config(command=_scroll)
         canvas.xview_moveto(0)
         canvas.yview_moveto(0)
-        self.interior = interior = tk.Frame(canvas)
         interior_id = canvas.create_window(0, 0, anchor=tk.NW, window=interior)
 
         def _configure_interior(event):
@@ -18,6 +24,8 @@ class   VerticalScrolledFrame(tk.Frame):
             canvas.config(scrollregion="0 0 %s %s" % size)
             if interior.winfo_reqwidth() != canvas.winfo_width():
                 canvas.config(width=interior.winfo_reqwidth())
+            if self.scrollAuto:
+                self.scrollDown()
         interior.bind("<Configure>", _configure_interior)
 
         def _configure_canvas(event):
@@ -29,4 +37,10 @@ class   VerticalScrolledFrame(tk.Frame):
 
     def addWidget(self, widgetFactory, *args, **kwargs):
         self.widgets.append(widgetFactory(self.interior, *args, **kwargs))
+        self.down = False
         return self.widgets[-1]
+
+    def scrollDown(self, check=True):
+        if check and self.down:
+            return
+        self.canvas.yview("moveto", "1.0")
